4402e7e24b586d2600aff90036ba6db37ac08ca5
Muhamad Rifqi Kurniawan <m.rifki0729@gmail.com>
2026-04-22 21:24:13 +0700

    feat: add header component for admin

---
+++ frontend/src/components/admin/Header.jsx
@@ -0,0 +1,24 @@
+export default function Header({ headerTitle }) {
+    return (
+        <header className="navbar shadow-sm px-8">
+            <div className="navbar-start">
+                <h2 className="text-3xl font-semibold">{ headerTitle }</h2>
+            </div>
+            <div className="navbar-end gap-2">
+                <h3 className="text-sm font-semibold">Halo, Admin!</h3>
+                <div className="dropdown dropdown-end">
+                    <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+                        <div className="w-10 rounded-full">
+                            <img src="/placeholder/profile_picture.png" />
+                        </div>
+                    </div>
+                    <ul tabIndex={-1} className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+                        <li><a>Profil</a></li>
+                        <li><a>Pengaturan</a></li>
+                        <li><a>Keluar</a></li>
+                    </ul>
+                </div>
+            </div>
+        </header>
+    )
+}
