39e7d539e57274cd8655f9977d6e0c955c452dae
Muhamad Rifqi Kurniawan <m.rifki0729@gmail.com>
2026-04-26 15:50:17 +0700

    feat: add new route for project page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { PublicLayout, AdminLayout } from "./layouts";
 
 import { LandingPage } from "./pages/public";
-import { DashboardAdmin } from "./pages/admin";
+import { DashboardAdmin, ProjectPage } from "./pages/admin";
 
 export default function App() {
   return (
@@ -14,6 +14,7 @@ export default function App() {
         </Route>
         <Route element={ <AdminLayout /> }>
           <Route path="/admin/dashboard" element={ <DashboardAdmin /> }></Route>
+          <Route path="/admin/projects" element={ <ProjectPage /> }></Route>
         </Route>
       </Routes>
     </BrowserRouter>
