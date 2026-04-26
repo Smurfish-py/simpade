a525ef8ec43e92b1eadc24ad27683737de48920a
Muhamad Rifqi Kurniawan <m.rifki0729@gmail.com>
2026-04-26 13:56:45 +0700

    feat: add export for chart component

--- frontend/src/components/admin/index.js
+++ frontend/src/components/admin/index.js
@@ -1,2 +1,3 @@
 export { default as AdminHeader } from '@/components/admin/Header';
 export { default as AdminModal } from '@/components/admin/Modal';
+export { default as AdminChart } from '@/components/admin/Chart';
