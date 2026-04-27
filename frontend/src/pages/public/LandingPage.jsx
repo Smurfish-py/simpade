37e5d7526742a30a53495944e4f21989df84975b
Muhamad Rifqi Kurniawan <m.rifki0729@gmail.com>
2026-04-28 06:54:17 +0700

    add header and footer structure to LandingPage component

--- frontend/src/pages/public/LandingPage.jsx
+++ frontend/src/pages/public/LandingPage.jsx
@@ -1,7 +1,32 @@
 export default function LandingPage() {
     return (
-        <main>
-            <div></div>
-        </main>
+        <>
+            <header className="navbar bg-base-100 shadow-sm">
+                <div className="navbar-start">
+                    <a href="/">
+                        <img src="/logo/simpade_logo.png" alt="Logo Simpade" width={152} />
+                    </a>
+                </div>
+                <div className="navbar-center">
+                    <ul className="menu menu-horizontal">
+                        <li><a>Beranda</a></li>
+                        <li><a>Proyek</a></li>
+                        <li><a>Aspirasi</a></li>
+                    </ul>
+                </div>
+                <div className="navbar-end">
+                    <div className="space-x-1">
+                        <a className="btn btn-ghost">Masuk</a>
+                        <a className="btn btn-primary">Daftar</a>
+                    </div>
+                </div>
+            </header>
+            <main>
+
+            </main>
+            <footer>
+
+            </footer>
+        </>
     )
 }
