0013a1e530197342d979dfedc0d235260a038bf5
Muhamad Rifqi Kurniawan <m.rifki0729@gmail.com>
2026-04-30 20:01:48 +0700

    fix: update profile link in Header component for correct navigation

--- frontend/src/components/admin/Header.jsx
+++ frontend/src/components/admin/Header.jsx
@@ -18,7 +18,7 @@ export default function Header({ headerTitle }) {
                         </div>
                     </div>
                     <ul tabIndex={-1} className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-                        <li><a>Profil</a></li>
+                        <li><a href="/profile">Profil</a></li>
                         <li><a>Pengaturan</a></li>
                         <li><a>Keluar</a></li>
                     </ul>
