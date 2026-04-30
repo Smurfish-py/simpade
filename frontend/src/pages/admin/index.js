7d0acc571e8e37243a5c596b449b2284f660d51f
Muhamad Rifqi Kurniawan <m.rifki0729@gmail.com>
2026-04-30 20:02:47 +0700

    fix: ensure AdminAspirations is exported from admin index

--- frontend/src/pages/admin/index.js
+++ frontend/src/pages/admin/index.js
@@ -1,2 +1,3 @@
 export { default as DashboardAdmin } from '@/pages/admin/Dashboard';
 export { default as ProjectAdmin } from '@/pages/admin/Projects';
+export { default as AdminAspirations } from '@/pages/admin/Aspirasi';
