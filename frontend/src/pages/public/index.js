6fedcd1ec79452510f53c106d6f19d2a593d5051
Muhamad Rifqi Kurniawan <m.rifki0729@gmail.com>
2026-04-30 20:03:55 +0700

    fix: update exports in public index to include all pages

--- frontend/src/pages/public/index.js
+++ frontend/src/pages/public/index.js
@@ -1,1 +1,8 @@
+export { default as DashboardPage } from '@/pages/public/Dashboard';
+export { default as ProjectPage } from '@/pages/public/Projects';
+export { default as LoginPage } from '@/pages/public/Login';
+export { default as RegisterPage } from '@/pages/public/Register';
+export { default as ForgotPasswordPage } from '@/pages/public/ForgotPassword';
+export { default as FeedbackPage } from '@/pages/public/Feedback';
 export { default as LandingPage } from '@/pages/public/LandingPage';
+export { default as ProfilePage } from '@/pages/public/Profile';
